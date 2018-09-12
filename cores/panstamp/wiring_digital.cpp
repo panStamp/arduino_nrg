8d9b9ab62a58449788187cb82b580f80a7ff8d7e
dberenguer <dberenguer@usapiens.com>
2018-09-12 11:51:53 +0200

    Adding pinMode_int

--- cores/panstamp/wiring_digital.cpp
+++ cores/panstamp/wiring_digital.cpp
@@ -26,6 +26,15 @@
 #include "pins.h"
 #include "wiring.h"
 
+#define bit_pos(A) ((A) == 1u << 0 ? 0 \
+: (A) == 1u << 1 ? 1 \
+: (A) == 1u << 2 ? 2 \
+: (A) == 1u << 3 ? 3 \
+: (A) == 1u << 4 ? 4 \
+: (A) == 1u << 5 ? 5 \
+: (A) == 1u << 6 ? 6 \
+: (A) == 1u << 7 ? 7 \
+: 0)
 
 /**
  * pinMode
@@ -68,6 +77,98 @@ void pinMode(uint8_t pin, uint8_t mode)
     default:
       break;
   }
+}
+
+/**
+ * pinMode_int
+ * 
+ * Config pin mode with additional working modes
+ * 
+ * @param pin pin number
+ * @param mode pin mode
+ */
+void pinMode_int(uint8_t pin, uint16_t mode)
+{
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+
+	volatile uint8_t *dir;
+	volatile uint8_t *ren;
+	volatile uint8_t *out;
+	volatile uint8_t *sel;
+
+	if (port == NOT_A_PORT) return;
+
+	dir = portDirRegister(port);
+	ren = portRenRegister(port);
+	out = portOutputRegister(port);
+
+	if (mode & OUTPUT) {
+		*dir |= bit;
+	} else {
+		*dir &= ~bit;
+		if (mode & INPUT_PULLUP) {
+			*out |= bit;
+			*ren |= bit;
+		} else if (mode & INPUT_PULLDOWN) {
+			*out &= ~bit;
+			*ren |= bit;
+		} else {
+	        *ren &= ~bit;
+		}
+	}
+
+#if (defined(P1SEL_) || defined(P1SEL) || defined(__MSP430_HAS_P1SEL__))
+	sel = portSel0Register(port);	/* get the port function select register address */
+	if (mode & PORT_SELECTION0) {
+		*sel |= bit;
+	} else {
+		*sel &= ~bit;
+	}
+#endif
+#if (defined(P1SEL2_) || defined(P1SEL2) || defined(__MSP430_HAS_P1SEL2__))
+	sel = portSel2Register(port);	/* get the port function select register address */
+	if (mode & PORT_SELECTION1) {
+		*sel |= bit;
+	} else {
+		*sel &= ~bit;
+	}
+#endif
+#if (defined(P1SEL0_) || defined(P1SEL0) || defined(__MSP430_HAS_P1SEL0__))
+	sel = portSel0Register(port);	/* get the port function select register address */
+	if (mode & PORT_SELECTION0) {
+		*sel |= bit;
+	} else {
+		*sel &= ~bit;
+	}
+#endif
+#if (defined(P1SEL1_) || defined(P1SEL1) || defined(__MSP430_HAS_P1SEL1__))
+	sel = portSel1Register(port);	/* get the port function select register address */
+	if (mode & PORT_SELECTION1) {
+		*sel |= bit;
+	} else {
+		*sel &= ~bit;
+	}
+#endif
+#if defined(__MSP430_HAS_PORT_MAPPING__)
+	volatile uint8_t *pmreg;
+	pmreg = portPMReg(port);
+
+	if(pmreg == NOT_A_PIN) return;
+
+	// Store current interrupt state, then disable all interrupts, to avoid that the port map is put into read only mode
+	uint16_t globalInterruptState = __get_interrupt_state();
+	__disable_interrupt();
+
+	PMAPKEYID = PMAPKEY;
+	PMAPCTL |= PMAPRECFG;
+	*(pmreg + bit_pos(bit)) = (mode >> 8) & 0xff;
+	// Make port map control read only by writing invalid password
+	PMAPKEYID = 0x0;
+
+	// Restore previous interrupt state
+	__set_interrupt_state(globalInterruptState);
+#endif
 }
 
 /**
